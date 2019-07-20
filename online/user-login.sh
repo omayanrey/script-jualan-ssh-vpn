b1904841764ae4290c815386e6ca617b43d6fde4
Rey Omayan <30548067+omayanrey@users.noreply.github.com>
2019-07-20 08:39:22 +0800

    Update user-login.sh

--- online/user-login.sh
+++ online/user-login.sh
@@ -7,7 +7,7 @@ do
 	#echo "check $PID";
 	NUM=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
 	USER=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
-	IP="xxxxxxxxx";
+	IP="xxxxxxxxxx";
 	if [ $NUM -eq 1 ]; then
 		echo "$PID - $USER - $IP";
 	fi
