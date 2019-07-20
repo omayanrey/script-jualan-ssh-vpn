217d70a871f36fbeccfb320fd87fafa608ae6ad5
Rey Omayan <30548067+omayanrey@users.noreply.github.com>
2019-07-20 08:03:22 +0800

    Create user-login.sh

---
+++ online/user-login.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
+
+for PID in "${data[@]}"
+do
+	#echo "check $PID";
+	NUM=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+	USER=`cat /var/log/secure | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
+	IP="SNAME";
+	if [ $NUM -eq 1 ]; then
+		echo "$PID - $USER - $IP";
+	fi
+done
+
